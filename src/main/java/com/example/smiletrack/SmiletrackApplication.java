7a0a40e56022c2306758313d1d4a2e5ab23ab05a
May <may.code.28@gmail.com>
2026-02-18 12:07:18 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/smiletrack/SmiletrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.smiletrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmiletrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmiletrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/smiletrack/SmiletrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smiletrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmiletrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
